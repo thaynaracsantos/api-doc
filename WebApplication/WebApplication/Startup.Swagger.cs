aea23ddfa2bc369fc2837c6ff0a00ea4b6f8937a
Thaynara Santos <thaynara.santos@mundipagg.com>
2017-09-03 00:57:34 -0300

    :heavy_plus_sign: Add swagger configuration

--- WebApplication/WebApplication/Startup.Swagger.cs
+++ WebApplication/WebApplication/Startup.Swagger.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
+using System.Reflection;
 
 namespace WebApplication
 {
@@ -8,12 +12,34 @@ namespace WebApplication
     {
         private void ConfigureSwaggerService(IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Web API",
+                        Version = "v1",
+                        Description = "Web API Description",
+                        TermsOfService = "Terms of service"
+                    }
+                 );
 
+                string basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                string moduleName = GetType().GetTypeInfo().Module.Name.Replace(".dll", ".xml");
+                string filePath = Path.Combine(basePath, moduleName);
+
+                options.IncludeXmlComments(filePath);
+                options.DescribeAllEnumsAsStrings();
+            });
         }
 
         private void ConfigureSwagger(IApplicationBuilder app, IHostingEnvironment env)
         {
-
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger(c => c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs"));
+            }
         }
     }
 }

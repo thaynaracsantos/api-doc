6e574a7ee4f1099878defa8aa4f64035b765913a
Thaynara Santos <thaynara.santos@mundipagg.com>
2017-09-03 02:21:18 -0300

    :heavy_plus_sign: Allow access to the documentation, even if you already have an authentication middleware

--- WebApplication/WebApplication/Controllers/DocsController.cs
+++ WebApplication/WebApplication/Controllers/DocsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace WebApplication.Controllers
     public class DocsController : Controller
     {
         [Route(""), HttpGet]
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
